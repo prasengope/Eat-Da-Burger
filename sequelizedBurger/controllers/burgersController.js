673354a6de7b37698f395ef52cb4f883df7f0c5d
Prasen Gope <prasen.gope@hotmail.com>
2019-05-18 00:15:02 -0400

    app now functional

--- sequelizedBurger/controllers/burgersController.js
+++ sequelizedBurger/controllers/burgersController.js
@@ -1,31 +1,41 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
+
+var db = require("../models/");
 
 router.get("/api/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
-    res.json(burgerData);
+  db.Burger.findAll()
+    .then(function(burgerData){
+      res.json(burgerData);
+    })
   });
-});
 
 // post route
 router.post("/api/burgers", function(req, res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    console.log(result);
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
     // Send back the ID of the new quote
+    .then(function(result){
     res.json({ id: result.insertId });
   });
 });
 
 // put route
 router.put("/api/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
-    console.log(result);
-    // Send back response and let page reload from .then in Ajax
-    res.sendStatus(200);
+  db.Burger.update({
+    devoured: true
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(result){
+        res.sendStatus(200);
   });
 });
 
--- sequelizedBurger/server.js
+++ sequelizedBurger/server.js
@@ -1,25 +1,24 @@
-// Dependencies
 var express = require("express");
 
-// Create an instance of the express app.
+// bring in the models
+var db = require("./models");
+
 var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Import routes and give the server access to them.
 var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+// listen on port 8080
+var PORT = process.env.PORT || 8080;
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening on port:", PORT);
+  });
 });
